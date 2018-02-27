e0d00a26ccb19a43d50bf749164660f31b3c7289
code4GG <cavallaro.av@gmail.com>
2018-02-26 23:38:18 -0500

    display letter check

--- assets/javascript/hang.js
+++ assets/javascript/hang.js
@@ -2,38 +2,97 @@
 	let lettersGuessed = [];
 	let wins = 0;
 	let guessesLeft = 10;
-	let wordArr = ['React','Angular','Vue'];
+	let wordArr = ['react','angular','vue'];
 	let randomWord = wordArr[Math.floor(Math.random()*wordArr.length)];
 	let letterArr = randomWord.split("");
 	let blanksArr = [];
-
+	
 	function blanks(){
 		for (let i = 0; i < letterArr.length; i++){
 			blanks = "_";
 			blanksArr.push(blanks);
 		}
 		console.log(blanksArr);
-		$('#blanks').text(blanksArr.join(" "));
+		$('#blanks').html(blanksArr.join(" "));
 	}
+
 	blanks();
 
+
 	function events(){
+
 		window.addEventListener('keyup', function(e){
 			$('#keystart').hide();
-
+			const regex = /^[A-z]+$/i;
 			const userGuess = e.key;
+			
+			const check = lettersGuessed.find(function(letter){
+				if (letter === userGuess){
+					return true;
+				}
+			})				
+		
+
+			console.log(check);
+			
 			console.log(userGuess); 
-
-			lettersGuessed.push(userGuess);
-			$('#lettersGuessed').text("Letters Already Guessed: " + lettersGuessed.join(" ").toUpperCase());
-			$('#remaining').text('Number of Guesses Remaining: ' + guessesLeft);
-
-			if (letterArr.indexOf(userGuess) > -1){
-				console.log('hi');
-			} else {
-				guessesLeft === guessesLeft--;
+			console.log(letterArr.indexOf(userGuess));
+				
+				// console.log(lettersGuessed);
+			if (check === undefined){
+				lettersGuessed.push(userGuess);	
+				$('#lettersGuessed').text("Letters Already Guessed: " + lettersGuessed.join(" ").toUpperCase());
+				$('#remaining').text('Number of Guesses Remaining: ' + guessesLeft);
+				$('#textdisplay').hide();
+			} 
+			else {
+				$('#textdisplay').show();
+				$('#textdisplay').text("You already guessed that letter try again.")
 			}
 
+
+			// if (letterArr.indexOf(e) > -1){
+			// 	for (let i = 0; i < blanksArr.length; i++){
+			// 		if (letterArr[i] === e){
+			// 			blanksArr[i] === e;
+			// 			$('#blanks').html(userGuess);
+			// 		}
+			// 	}
+			// }
+			// 	$('#blanks').text(blanksArr.join(" "));
+			// }
+			if (userGuess === letterArr){
+				for (let i = 0; i < letterArr.length; i++) {
+					console.log("hi");
+				}
+			}
+			// 	console.log('found');
+			// } else {
+			// 	guessesLeft === guessesLeft--;
+			// }
+			// console.log(blanksArr);
+		  	
+
+		// function checkWin() {
+		//   if (correctGuesses.indexOf('_') === -1) {
+		//     alert('You Won!');
+		//   } else if (allowedGuesses === 0) {
+		//     alert('You Lost!');
+		//   }
+		// }
+	
 		})
 	}
 	events();
+
+	// function check(event){
+	// 		if (letterArr.indexOf(e) > -1){
+	// 			for (let i = 0; i < blanksArr.length; i++){
+	// 				if (letterArr[i] === e){
+	// 					blanksArr[i] === e;
+	// 					$('#blanks').html(blanksArr.join(" "));
+	// 				}
+	// 			}
+	// 		}
+	// }
+	// check();
